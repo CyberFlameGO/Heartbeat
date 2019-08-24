9803c3a3e88e071face18980a638abebf767e80a
George <george.marr.cobc@gmail.com>
2019-08-24 17:35:54 +0100

    oops

--- main/Heartbeat.go
+++ main/Heartbeat.go
@@ -1,7 +1,33 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
+type Stats struct {
+	User, Nice, System, Idle, Iowait, Irq, Softirq, Steal, Guest, GuestNice, Total uint64
+	CpuCount, StatCount                                                            uint64
+}
+
+type cpuStat struct {
+	name string
+	ptr  *uint64
+}
+
+func GetCpu() (*Stats, error) {
+	file, err := os.Open("/proc/stat")
+
+	if err != nil {
+		fmt.Println("Could not read /proc/stat, found error: ", err.Error())
+		return nil, err
+	}
+
+	defer file.Close()
+
+	//todo return getting the stats
+}
+
 func main() {
 
 }
-
-j
