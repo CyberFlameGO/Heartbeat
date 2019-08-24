dcc596df0b32ec6d982e60b25257541afd65ceac
George <george.marr.cobc@gmail.com>
2019-08-24 22:11:52 +0100

    setup the base for memory

--- main/memory.go
+++ main/memory.go
@@ -1,1 +1,26 @@
 package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+type MemoryStats struct {
+	Total, Used, Buffers, Cached, Free, Available, Active, Inactive uint64
+}
+
+func GetMemory() (*MemoryStats, error) {
+	file, err := os.Open("/proc/meminfo")
+
+	if err != nil {
+		return nil, fmt.Errorf("Could not open /proc/meminfo, found error: %s", err)
+	}
+
+	defer file.Close()
+	return getMemoryStats(file)
+}
+
+func getMemoryStats(out io.Reader) (*MemoryStats, error) {
+
+}
