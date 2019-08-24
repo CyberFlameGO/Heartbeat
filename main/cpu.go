6a5ffaba1e098f3a38b129a539a9910aa4b0ffd2
George <george.marr.cobc@gmail.com>
2019-08-24 21:49:38 +0100

    ffs goland

--- main/cpu.go
+++ main/cpu.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 type CPUStats struct {
 	User, System, Idle, Total uint64
 	CPUCount, StatCount       uint64
@@ -8,4 +13,17 @@ type CPUStats struct {
 type CPUInfo struct {
 	name string
 	ptr  *uint64
+}
+
+func GetCpuStats() (*CPUStats, error) {
+	file, err := os.Open("/proc/stat")
+
+	if err != nil {
+		_ = fmt.Errorf("Could not read /proc/stat, found error: %s", err)
+		return nil, err
+	}
+
+	defer file.Close()
+
+	//todo return
 }
