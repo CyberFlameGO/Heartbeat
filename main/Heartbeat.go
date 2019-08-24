9163973a698f8aaa01721970dbccbf39217b21c8
George <george.marr.cobc@gmail.com>
2019-08-24 17:22:58 +0100

    add in getting the cpu usage

--- main/Heartbeat.go
+++ main/Heartbeat.go
@@ -1,22 +1,47 @@
 package main
 
 import (
-	"errors"
+	"fmt"
 	"io/ioutil"
+	"strconv"
+	"strings"
 )
 
 func main() {
 
 }
 
-//cpu usage
-
+//get the cpu usage and the idle time
 func getCPUUsage() (idle, total uint64) {
 	contents, err := ioutil.ReadFile("/proc/stat")
 
 	if err != nil {
-		errors.New("Could not read /proc/stat")
-		err.Error()
+		fmt.Println("Could not read /proc/stat found error: ", err)
 		return
 	}
+
+	lines := strings.Split(string(contents), "\n")
+
+	for line := range lines {
+		fields := strings.Fields(string(line))
+		if fields[0] == "cpu" {
+			fieldCount := len(fields)
+
+			for i := 1; i < fieldCount; i++ {
+				val, err := strconv.ParseUint(fields[i], 10, 64)
+
+				if err != nil {
+					total += val
+
+					if i == 4 {
+						idle = val
+					}
+				}
+
+				return
+			}
+		}
+	}
+
+	return
 }
