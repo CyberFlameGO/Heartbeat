1ef523e15727b075c879287a3bfdfd47826fe531
George <george.marr.cobc@gmail.com>
2019-08-24 22:17:05 +0100

    time to build it properly

--- main/Heartbeat.go
+++ main/Heartbeat.go
@@ -10,5 +10,12 @@ func main() {
 		fmt.Println("[X] Could not start Heartbeat due to not being on linux, aborting!")
 		runtime.StopTrace()
 		return
+	} else {
+
+		var cpu, _ = GetCpuStats()
+		var memory, _ = GetMemory()
+		var disk, _ = GetDiskStats()
+
+		fmt.Println("{==== HEARTBEAT ====}")
 	}
 }
