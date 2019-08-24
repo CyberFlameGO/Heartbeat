de1ccc72c7913b89de3149e21af26a586a02ae58
George <george.marr.cobc@gmail.com>
2019-08-24 22:16:15 +0100

    get the memory information

--- main/memory.go
+++ main/memory.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"io"
 	"os"
+	"strconv"
+	"strings"
 )
 
 type MemoryStats struct {
@@ -22,5 +25,46 @@ func GetMemory() (*MemoryStats, error) {
 }
 
 func getMemoryStats(out io.Reader) (*MemoryStats, error) {
+	scanner := bufio.NewScanner(out)
+	var memory MemoryStats
 
+	memStats := map[string]*uint64{
+		"MemTotal":     &memory.Total,
+		"MemFree":      &memory.Free,
+		"MemAvailable": &memory.Available,
+		"Buffers":      &memory.Buffers,
+		"Cached":       &memory.Cached,
+		"Active":       &memory.Active,
+		"Inactive":     &memory.Inactive,
+	}
+
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		i := strings.IndexRune(line, ':')
+
+		if i < 0 {
+			continue
+		}
+
+		f := line[:1]
+
+		ptr := memStats[f]
+
+		if ptr != nil {
+			val := strings.TrimSpace(strings.TrimRight(line[i+1:], "kb"))
+
+			if v, err := strconv.ParseUint(val, 10, 64); err == nil {
+				*ptr = v * 1024
+			}
+		}
+	}
+
+	err := scanner.Err()
+
+	if err != nil {
+		return nil, fmt.Errorf("Could not scan /proc/meminfo found error: %s", err)
+	}
+
+	return &memory, nil
 }
