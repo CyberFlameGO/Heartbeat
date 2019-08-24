fec7c227a082bf8edef1a3c7242bbc010def7833
George <george.marr.cobc@gmail.com>
2019-08-24 17:20:05 +0100

    lets build this then

--- main/Heartbeat.go
+++ main/Heartbeat.go
@@ -1,1 +1,22 @@
 package main
+
+import (
+	"errors"
+	"io/ioutil"
+)
+
+func main() {
+
+}
+
+//cpu usage
+
+func getCPUUsage() (idle, total uint64) {
+	contents, err := ioutil.ReadFile("/proc/stat")
+
+	if err != nil {
+		errors.New("Could not read /proc/stat")
+		err.Error()
+		return
+	}
+}
