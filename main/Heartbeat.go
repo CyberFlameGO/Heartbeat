739fe09daf7c3234faae07d78ff88ee686ce6405
George <george.marr.cobc@gmail.com>
2019-08-24 21:45:29 +0100

    add in a check to make sure that the OS is linux

--- main/Heartbeat.go
+++ main/Heartbeat.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"runtime"
+)
 
+func main() {
+	if runtime.GOOS != "linux" {
+		fmt.Println("[X] Could not start Heartbeat due to not being on linux, aborting!")
+		runtime.StopTrace()
+		return
+	}
 }
